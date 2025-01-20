be9c286313666d26dc41c6fb7e19d7f6933ae8dd
Mano Kiku <nunununununununununuunu@gmail.com>
2025-01-20 19:58:23 +0300

    Added hp, power and bomb to player, also added radial bullet spawner

---
+++ Assets/Scripts/Enemy/RadialBulletController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class RadialBulletController : MonoBehaviour
+{
+    [Header("Projectile Settings")]
+    public float projectileSpeed;               // Speed of the projectile.
+    public GameObject ProjectilePrefab;         // Prefab to spawn
+    public float additionalAngle = 0;
+
+    [Header("Private Variables")]
+    private const float radius = 1F;            // Help us find the move direction.
+
+    // Spawns x number of projectiles.
+    public void SpawnProjectile(int _numberOfProjectiles)
+    {
+        float angleStep = 360f / _numberOfProjectiles;
+        float angle = 0f;
+
+        for (int i = 0; i <=_numberOfProjectiles -1; i++)
+        {
+            // Direction calculations.
+            float projectileDirXPosition = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+            float projectileDirYPosition = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+
+
+            // Create vectors.
+            Vector2 projectileVector = new Vector2(projectileDirXPosition, projectileDirYPosition);
+            Vector2 projectileMoveDirection = (projectileVector - new Vector2(transform.position.x, transform.position.y)).normalized * projectileSpeed;
+
+            // Create game objects.
+            GameObject tmpObj = Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
+            tmpObj.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
+
+            // Destory the gameobject after 10 seconds.
+            Destroy(tmpObj, 10F);
+
+            angle += angleStep;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public static PlayerStatus instance;
+
+    [Header("Main stats")]
+    public int hp;
+    public int bombAmount;
+    public int powerAmount;
+    private bool isInvincible = false;
+
+    [Header("Private var's")]
+    [SerializeField]
+    private float bombRadius = 3;
+
+
+    private void Awake() {
+        instance = instance ?? this;
+    }
+
+    public void TakeHP() {
+        if(isInvincible) {
+            return;
+        }
+
+        StartCoroutine(InvincibleSet(3));
+
+        hp--;
+        powerAmount = powerAmount * 5 / 6;
+    }
+
+    public void UseBomb() {
+        if(bombAmount <= 0) {
+            return;
+        }
+
+        List<Collider2D> objects = Physics2D.OverlapCircleAll(transform.position, bombRadius).ToList<Collider2D>();
+        foreach(var obj in objects) {
+            if(obj.CompareTag("Bullet")) {
+                Destroy(obj.gameObject);
+            }
+        }
+ 
+        bombAmount--;
+    }
+
+    private IEnumerator InvincibleSet(float time) {
+        isInvincible = true;
+        yield return new WaitForSeconds(time);
+        isInvincible = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Bullet")) {
+            Debug.Log("Lol");
+            TakeHP();
+        }    
+    }
+}
